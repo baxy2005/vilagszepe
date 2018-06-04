19bcae77f8be8eaf76a7f0c2c4f086ff5ef4cb06
andras varadi <andris@varadi.tv>
2018-06-04 18:27:20 +0200

    overlay when the navigation is open

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,17 +1,43 @@
 $(document).ready(function() {
+  var brand = $('.navmenu h3').html("világszép");
+  var brandLogo = $('.navmenu i img').html('<img src="images/brand-spiral.svg" alt="vilagszep">')
+
+if ( $(window).width() < 992 ) {
+  console.log('hello')
+  $('.navbar').append(brandLogo);
+  $('.navbar').append(brand);
+  console.log(brand)
+}
+
+
   if ( $(window).width() < 540 ) {
     startCarousel();
   } else {
     $('.owl-carousel').addClass('off');
   }
+  // overlay
+  $('.navmenu').on('show.bs.offcanvas', function() {
+  	if ($(window).width() <= 992) {
+  		$('.overlay').fadeIn();
+    }
+  });
+  $('.navmenu').on('hide.bs.offcanvas', function() {
+  	if ($(window).width() <= 992) {
+  		$('.overlay').fadeOut();
+    }
+  });
 });
-
 $(window).resize(function() {
     if ( $(window).width() < 540 ) {
       $('.owl-carousel').removeClass('off');
       startCarousel();
     } else {
       stopCarousel();
+    }
+    // close modal on resize
+    if ($(window).width() > 992) {
+      $('.overlay').hide(0, false);
+      $('body').removeClass('bs.offcanvas');
     }
 });
 
@@ -37,3 +63,28 @@ function stopCarousel() {
   owl.trigger('destroy.owl.carousel');
   owl.addClass('off');
 }
+
+// simulate modal opening
+// $('.nav-link').click(function() {
+//   if ($(window).width() > 992) {
+//     $('.overlay').hide(0, false);
+//   }
+//
+// 	$('#myNavmenu').click();
+// });
+
+
+
+
+
+
+// // switch active navigation link onclick
+// $('.nav a').on('click', function() {
+//   $('.nav').find('.active').removeClass('active');
+//   $(this).parent().addClass('active');
+// });
+//
+// // close modal when navigating to anchor
+// $('.navmenu-nav li a').on('click', function() {
+//   $('body').removeClass('bs.offcanvas');
+// });
